7d3b9bec1ddddc76950dbb42c45f7771c5ea63ec
tekgeek88 <tekgeek88@yahoo.com>
2018-11-16 23:55:04 -0800

    Final submit

--- simulator.c
+++ simulator.c
@@ -1,3 +1,9 @@
+/*
+ * TCSS 371 hw 4
+ * Ethan Cheatham
+ * Carl Argabright
+ * We used the CLion ide to complete this project
+ */
 #include <stdio.h>
 
 #include "bstr.h"
@@ -171,6 +177,46 @@ void testAddImmediateMode(int a, int b) {
 }
 
 
+/* Tests for all of the add instruction cases */
+void testAdd() {/* Test the ADD instruction in Register mode with two positive numbers */
+    testAddRegisterMode(5, 5);
+
+    /* Test the ADD instruction in Register mode with two negative numbers */
+    testAddRegisterMode(-5, -5);
+
+    /* Test the ADD instruction in Register mode with a negative and positive number
+     * result is negative  */
+    testAddRegisterMode(-5, 2);
+
+    /* Test the ADD instruction in Register mode with a negative and positive number
+     * result is positive  */
+    testAddRegisterMode(-5, 10);
+
+    /* Test the ADD instruction in Register mode with a negative and positive number
+     * result is zero  */
+    testAddRegisterMode(-5, 5);
+
+
+    /* Test the ADD instruction in Immediate mode with two positive numbers */
+    testAddImmediateMode(5, 5);
+
+    /* Test the ADD instruction in Immediate mode with two negative numbers */
+    testAddImmediateMode(-5, -5);
+
+    /* Test the ADD instruction in Immediate mode with a negative and positive number
+     * result is negative  */
+    testAddImmediateMode(-5, 2);
+
+    /* Test the ADD instruction in Immediate mode with a negative and positive number
+     * result is positive  */
+    testAddImmediateMode(-5, 10);
+
+    /* Test the ADD instruction in Immediate mode with a negative and positive number
+     * result is zero  */
+    testAddImmediateMode(-5, 5);
+}
+
+
 /***
  * Tests the load instruction
  * given a destination register and a 9bit signed PCoffset
@@ -179,7 +225,7 @@ void testAddImmediateMode(int a, int b) {
  * @param offset 9 bit int representing the memory address to fetch the data to be loaded.
  * @param initialPC 16 bit int representing the desired initial PC value.
  */
-void testLdMode(int dest_register, int offset, int initialPC) {
+void testLdInstruction(int dest_register, int offset, int initialPC) {
 
     printf("\n\n\t\t####  Starting LD test  ####\n");
 
@@ -192,7 +238,7 @@ void testLdMode(int dest_register, int offset, int initialPC) {
     /* Load some interesting things into the all of the memory registers */
     int i;
     for (i = 0; i < 50; i++) {
-            BSTR_SetValueTwosComp(&(comp.mem[i]), i, 16);
+        BSTR_SetValueTwosComp(&(comp.mem[i]), i, 16);
     }
 
     /* Create desired instruction set */
@@ -229,53 +275,7 @@ void testLdMode(int dest_register, int offset, int initialPC) {
     printf("%d + %d\n", initialPC, offset);
 }
 
-
-int main(int argc, const char * argv[]) {
-
-    /* Tests the NOT instruction */
-    //    testNot();
-
-    /* Test the ADD instruction in Register mode with two positive numbers */
-//    testAddRegisterMode(5, 5);
-
-    /* Test the ADD instruction in Register mode with two negative numbers */
-//    testAddRegisterMode(-5, -5);
-
-    /* Test the ADD instruction in Register mode with a negative and positive number
-     * result is negative  */
-//    testAddRegisterMode(-5, 2);
-
-    /* Test the ADD instruction in Register mode with a negative and positive number
-     * result is positive  */
-//    testAddRegisterMode(-5, 10);
-
-    /* Test the ADD instruction in Register mode with a negative and positive number
-     * result is zero  */
-//    testAddRegisterMode(-5, 5);
-
-
-    /* Test the ADD instruction in Immediate mode with two positive numbers */
-//    testAddImmediateMode(5, 5);
-
-    /* Test the ADD instruction in Immediate mode with two negative numbers */
-//    testAddImmediateMode(-5, -5);
-
-    /* Test the ADD instruction in Immediate mode with a negative and positive number
-     * result is negative  */
-//    testAddImmediateMode(-5, 2);
-
-    /* Test the ADD instruction in Immediate mode with a negative and positive number
-     * result is positive  */
-//    testAddImmediateMode(-5, 10);
-
-    /* Test the ADD instruction in Immediate mode with a negative and positive number
-     * result is zero  */
-//    testAddImmediateMode(-5, 5);
-
-
-
-    /* Test LD instruction */
-
+void testLd() {
     int destRegister, pcOffset, initialPC;
 
     /* Test LD into register 0 with no offset and PC at 0 */
@@ -283,7 +283,7 @@ int main(int argc, const char * argv[]) {
     destRegister = 0;   // Load into
     pcOffset = 0;       // This number of steps ahead
     initialPC = 0;      // Starting at PC + 1
-    testLdMode(destRegister, pcOffset, initialPC);
+    testLdInstruction(destRegister, pcOffset, initialPC);
 
 
     /* Test LD into register 0 with positive offset and PC at 0 */
@@ -291,7 +291,7 @@ int main(int argc, const char * argv[]) {
     destRegister = 0;   // Load into
     pcOffset = 10;       // This number of steps ahead
     initialPC = 0;      // Starting at PC + 1
-    testLdMode(destRegister, pcOffset, initialPC);
+    testLdInstruction(destRegister, pcOffset, initialPC);
 
 
     /* Test LD into register 0 with negative offset and PC at 10 */
@@ -299,104 +299,226 @@ int main(int argc, const char * argv[]) {
     destRegister = 0;   // Load into
     pcOffset = -5;       // This number of steps ahead
     initialPC = 10;      // Starting at PC + 1
-    testLdMode(destRegister, pcOffset, initialPC);
-
-
-    /************************************** */
-/** The next two variables - program and programSize - */
-/** allow someone using the simulator (such as a grader) */
-/** to decide what program will be simulated. */
-/** The simulation must load and execute */
-/** instructions found in the "program" array. */
-/** For grading purposes, it must be possible for me to */
-/**    - paste in a different set of binary strings to replace the existing ones */
-/**    - adjust the programSize variable */
-/**    - recompile your program without further changes */
-/**    and see the simulator load and execute the new program. */
-/**    Your grade will depend largely on how well that works. */
-/************************************** */
-
-//    char* program[] =
-//    {
-//     "0010000000000111",
-//     "0010001000000111",
-//     "0001010000000001",
-//     "0000010000000011",
-//     "1111000000100001",
-//     "0001000000111111",
-//     "0000111111111011",
-//     "1111000000100101",
-//     "0000000000111001",
-//     "1111111111010000"
-//	 };
-//
-//    int programSize = 10;
-
-//	BitString addInstr;
-
-    /*  You may find it useful to play around with the */
-    /*  BitString routines.  Some code that does that is provided */
-    /*  in the following commented out function. */
-
-//     bitStringTest();
-
-/*   This is the assembly program that was compiled into the binary
-     program shown above.
-    .ORIG x3000
-
-            LD   R0  START
-            LD   R1  END
-     TOP    ADD  R2  R0  R1
-            BRZ  DONE
-            OUT
-            ADD  R0 R0 -1
-            BRNZP TOP
-     DONE   HALT
-
-     START .FILL x39
-     END   .FILL x-30
-
-     .END
-*/
-
-
-//    BitString demoStr, sub;
-//
-//	//BSTR_SetValue(&demoStr,15,8);
-//	BSTR_SetBits(&demoStr,"0000000000001111");
-//
-//	BSTR_Display(demoStr,1);
-//
-//	BSTR_Substring(&sub,demoStr,2,4);
-//	BSTR_Display(sub,1);
-//
-//	COMP_Init(&comp);
-//	COMP_Display(comp);   /* displays computer's initial configuration
-//
-//	/* TO DO: load the instructions in the "program" array */
-//
-//	/* Next 3 lines are a test of NOT */
-//	/* Once you are confident that single instructions work, you will */
-//	/* want to replace this with code that loads all the instructions */
-//	/* from the array shown above. */
-//
-//	/* following not instruction is   NOT Dest: R4, Source: R5 */
-//    BitString addInstr;
-//	BSTR_SetBits(&addInstr,"1001100101111111");
-//	COMP_LoadWord(&comp,0,addInstr);
-//    COMP_Display(comp);
-//
-//	/* execute the program */
-//	/* During execution, the only output to the screen should be */
-//	/* the result of executing OUT. */
-//	COMP_Execute(&comp);
-//
-//	/* shows final configuration of computer */
-//	COMP_Display(comp);
-
+    testLdInstruction(destRegister, pcOffset, initialPC);
 }
 
 
+void testOutput() {
+    printf("Testing Output Operation\n");
+    int a = 5, b = 7;
+
+    /* Initialize computer */
+    Computer comp;
+    COMP_Init(&comp);
+
+    /* Create desired instructions */
+    BitString addInstrComplete; // R1 <-- R0 + b
+    BitString addInstr;         // R1 <-- R0 + imm5
+    BitString imm5BS;           // 5 bit 2's comp binary number
+    BitString haltInstr;
+    BitString outputInstr;
+    BSTR_SetValueTwosComp(&imm5BS, b, 5);
+
+    BSTR_SetBits(&addInstr, "00010010001"); // ADD Dest: R1, Src1: R0 Imm5: b
+    BSTR_Append(&addInstrComplete, addInstr, imm5BS);
+
+    COMP_LoadWord(&comp, 0, addInstrComplete);
+
+    BSTR_SetBits(&outputInstr, "1111000000100001");
+    COMP_LoadWord(&comp, 1, outputInstr);
+
+    BSTR_SetBits(&haltInstr, "1111000000100101");
+    COMP_LoadWord(&comp, 2, haltInstr);
+
+    /* Setup R0 with the given value of a */
+    BSTR_SetValueTwosComp(&(comp.reg[0]), a, 16); // R0 <-- a
+
+    /* Execute all loaded instructions */
+    COMP_Execute(&comp);
+
+    printf("Finished output test\n");
+}
+
+void testHalt() {
+
+    printf("Testing Halt Operation\n");
+    int a = 5, b = 7;
+
+    /* Initialize computer */
+    Computer comp;
+    COMP_Init(&comp);
+
+    /* Create desired instructions */
+    BitString addInstrComplete; // R1 <-- R0 + b
+    BitString addInstr;         // R1 <-- R0 + imm5
+    BitString imm5BS;           // 5 bit 2's comp binary number
+    BitString haltInstr;
+    BitString outputInstr;
+    BSTR_SetValueTwosComp(&imm5BS, b, 5);
+
+    BSTR_SetBits(&haltInstr, "1111000000100101");
+    COMP_LoadWord(&comp, 0, haltInstr);
+
+    BSTR_SetBits(&addInstr, "00010010001"); // ADD Dest: R1, Src1: R0 Imm5: b
+    BSTR_Append(&addInstrComplete, addInstr, imm5BS);
+
+    COMP_LoadWord(&comp, 1, addInstrComplete);
+
+    BSTR_SetBits(&outputInstr, "1111000000100001");
+    COMP_LoadWord(&comp, 2, outputInstr);
 
 
+    /* Setup R0 with the given value of a */
+    BSTR_SetValueTwosComp(&(comp.reg[0]), a, 16); // R0 <-- a
 
+    /* Display initialized computer and loaded instructions */
+    printf("\t####  Displaying computers initial state  ####\n");
+    COMP_Display(comp);
+
+    /* Execute all loaded instructions */
+    COMP_Execute(&comp);
+
+    /* Display state of computer after execution */
+    printf("\n\n\t####  Displaying computers state after execution ####\n");
+    COMP_Display(comp);
+
+    printf("\n\t\t####  Finished Halt Before Add test. Should be no output or addition occurring.  ####\n\n");
+}
+
+
+/***
+ * Tests the load instruction
+ * given a destination register and a 9bit signed PCoffset
+ *
+ * @param dest_register 3 bit int in the range of 1-7 representing the Destination.
+ * @param offset 9 bit int representing the memory address to fetch the data to be loaded.
+ * @param initialPC 16 bit int representing the desired initial PC value.
+ */
+void testBrInstruction(char* brNzp, char* setCC, int offset, int initialPC) {
+
+    printf("\n\n\t\t####  Starting BR test  ####\n");
+
+    /* Initialize computer */
+    Computer comp;
+    COMP_Init(&comp);
+
+    /* Set intial PC */
+    BSTR_SetValue(&(comp.pc),initialPC,16);
+
+    /* Set computers current CC flag */
+    BSTR_SetBits(&(comp.cc), setCC);
+
+    /* Load some interesting things into the all of the memory registers */
+    int i;
+    for (i = 0; i < 50; i++) {
+        BSTR_SetValueTwosComp(&(comp.mem[i]), i, 16);
+    }
+
+    /* Create desired instruction set */
+    BitString brInstr;
+    BSTR_SetBits(&brInstr, "0000");         // BR Dest: PC + PCoffset9
+    /* Create brNzp bitstring */
+    BitString nzpBS;
+    BSTR_SetBits(&nzpBS, brNzp);
+    /* Create the offset BS */
+    BitString pcOffset;                     // 9 bit 2's comp binary number
+    BSTR_SetValueTwosComp(&pcOffset, offset, 9);
+    /* Create the full instruction needed for LD operation */
+    BitString brInstrComplete;              // dest_register <-- M[PC + PCoffset9]
+    BSTR_Append(&brInstrComplete, brInstr, nzpBS);
+    BSTR_Append(&brInstrComplete, brInstrComplete, pcOffset);
+
+    BSTR_Display(brInstrComplete, 0);
+
+    /* Load desired instruction */
+    COMP_LoadWord(&comp, initialPC, brInstrComplete);
+
+    /* Display initialized computer and loaded instructions */
+    printf("\t####  Displaying computers initial state  ####\n");
+    COMP_Display(comp);
+
+    /* Execute all loaded instructions */
+    COMP_Execute(&comp);
+
+    //sets pc based on offset from branch (if conditions met)
+
+    /* Display state of computer after execution */
+    printf("\n\n\t####  Displaying computers state after execution ####\n");
+    COMP_Display(comp);
+
+    printf("\n\t\t####  Finished BR test  ####\n\n");
+    printf("Verify PC <-- PC + 1 + offset)\n");
+    printf("%d + %d\n", initialPC, offset);
+    printf("Verify Current PC: ");
+    BSTR_Display(comp.pc, 1);
+}
+
+void testBR() {
+    char* brCC = "111";
+    char* currentCC = "001";
+    int pcOffset = 1;
+    int initialPC = 1;
+    testBrInstruction(brCC, currentCC, 1, 1);
+}
+
+
+int main(int argc, const char * argv[]) {
+
+    Computer comp;
+    int programSize = 10;
+    BitString instruction;
+
+    char* program[] =
+            {
+                    "0010000000000111",
+                    "0010001000000111",
+                    "0001010000000001",
+                    "0000010000000011",
+                    "1111000000100001",
+                    "0001000000111111",
+                    "0000111111111011",
+                    "1111000000100101",
+                    "0000000000111001",
+                    "1111111111010000"
+            };
+
+
+    //initial state
+    COMP_Init(&comp);
+
+    //load instructions (bits) into computer
+    int i;
+    for (i = 0; i < programSize; i++) {
+        BSTR_SetBits(&instruction, program[i]);
+        COMP_LoadWord(&comp, i, instruction);
+    }
+
+    //execute the program
+    COMP_Execute(&comp);
+
+    //shows final configuration
+    COMP_Display(comp);
+
+    /* Tests the NOT instruction */
+//     testNot();
+
+    /* Test halt operation, prevent program from outputting...*/
+//        testHalt();
+
+    /* Test output operation */
+//        testOutput();
+
+    /* Test the ADD instructions */
+//     testAdd();
+
+    /* Test LD instructions */
+//     testLd();
+
+    /* Test BitString class */
+//     bitStringTest();
+
+    /* Test BR instruction */
+//    testBR();
+
+
+}
